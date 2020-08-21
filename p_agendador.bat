962d430d27089db6de949426db1e7eeaa8669f41
HENRCAST <henrique_castro@outlook.com>
2020-08-21 14:34:44 -0300

    initial

---
+++ p_agendador.bat
@@ -0,0 +1,8 @@
+if not "%minimized%"=="" goto :minimized
+set minimized=true
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:minimized
+rem Anything after here will run in a minimized window
+Rscript --vanilla tryCatch.R %CD%
+exit
